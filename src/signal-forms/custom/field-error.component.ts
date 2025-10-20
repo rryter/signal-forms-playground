700a6b75fa7e92645dfac5426a3f58c54fb891fb
Reto Ryter <reto.ryter@css.ch>
2025-10-20 20:35:20 +0200

    Pass FieldState signals to FieldError component and remove Name field
    - Rename FieldError input to `fieldState` and update template bindings to use `fieldState()`
    - Add FieldState<string, any> typing and include `computed` import
    - Update all <app-field-error> usages to pass `userForm.<field>()` via [fieldState]
    - Remove the "Name" form-group from the basic form template

--- src/signal-forms/custom/field-error.component.ts
+++ src/signal-forms/custom/field-error.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from "@angular/core";
+import { Component, input, computed } from "@angular/core";
 import { JsonPipe } from "@angular/common";
 import { FieldState } from "@angular/forms/signals";
 
@@ -7,13 +7,13 @@ import { FieldState } from "@angular/forms/signals";
   standalone: true,
   imports: [JsonPipe],
   template: `
-    @if (field().touched() && field().errors().length > 0) {
+    @if (fieldState().touched() && fieldState().errors().length > 0) {
       <div class="error">
-        <span>{{ field().errors() | json }}</span>
+        <span>{{ fieldState().errors() | json }}</span>
       </div>
     }
   `,
 })
 export class FieldError {
-  field = input.required<FieldState<any>>();
+  fieldState = input.required<FieldState<string, any>>();
 }
